942c29c38fcf76a6f7814321866ac80bb1d0a9f2
toffa <toffa@toffa.toffa>
2025-04-30 19:17:12 +1000

    test request

---
+++ cmd/tui/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/DevPutat/QwenTUI/internal/config"
+	"github.com/DevPutat/QwenTUI/internal/request"
+)
+
+func main() {
+	flagQuery := flag.String("query", "", "query for request")
+	flagKey := flag.String("key", "", "key for api")
+	flag.Parse()
+
+	c := config.Config(*flagKey)
+
+	if len(*flagQuery) > 0 {
+		fmt.Println(request.Send(*flagQuery, c))
+	}
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import "github.com/DevPutat/QwenTUI/internal/types"
+
+const (
+	//defaultApiURL = "https://api.qwen.com/v1/chat"
+	defaultApiURL = "https://openrouter.ai/api/v1/chat/completions"
+
+	defaultApiKey    = "your-api-key"
+	defaultModelName = "qwen/qwen3-235b-a22b:free"
+)
+
+func Config(key string) types.Conf {
+	c := types.Conf{
+		ApiKey:    defaultApiKey,
+		ApiURL:    defaultApiURL,
+		ModelName: defaultModelName,
+	}
+	if len(key) > 0 {
+		c.ApiKey = key
+	}
+	return c
+}
